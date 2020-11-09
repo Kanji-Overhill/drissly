dcac196d1627221a7ef5814a967c2f3a84d85da4
unknown <ingluisfelipe07@gmail.com>
2020-11-08 20:44:57 -0600

    first commit

---
+++ resources/views/home.blade.php
@@ -0,0 +1,116 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Home | 1 | Execoore | Technology And Business Template</title>
+    <meta name="description" content="">
+    <link rel="stylesheet" href="css/bootstrap-grid.css">
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="css/glide.css">
+    <link rel="stylesheet" href="css/magnific-popup.css">
+    <link rel="stylesheet" href="css/content-box.css">
+    <link rel="stylesheet" href="css/contact-form.css">
+    <link rel="stylesheet" href="css/media-box.css">
+    <link rel="stylesheet" href="css/skin.css">
+    <link rel="icon" href="media/favicon.png">
+</head>
+<body>
+    <div id="preloader"></div>
+    <nav class="menu-top-logo menu-fixed" data-menu-anima="fade-in">
+        <div class="container">
+            <div class="menu-brand">
+                <a href="#">
+                    <img class="logo-default scroll-hide" src="media/logo.svg" alt="logo" />
+                    <img class="logo-retina scroll-hide" src="media/logo.svg" alt="logo" />
+                    <img class="logo-default scroll-show" src="media/logo.svg" alt="logo" />
+                    <img class="logo-retina scroll-show" src="media/logo.svg" alt="logo" />
+                </a>
+            </div>
+            <i class="menu-btn"></i>
+            <div class="menu-cnt">
+                <ul id="main-menu">
+                    <li>
+                        <a href="#">Inicio</a>
+                    </li>
+                    <li>
+                        <a href="#">Acerca de</a>
+                    </li>
+                    <li>
+                        <a href="#">Preguntas Frecuentes</a>
+                    </li>
+                    <li>
+                        <a href="#">Contacto</a>
+                    </li>
+                    <li class="nav-label">
+                        <a href="#"><span>Call us:</span> (023) 112 589 </a>
+                    </li>
+                </ul>
+                <div class="menu-right">
+                    <ul class="lan-menu">
+                        <li>
+                            <a href="#"><img src="media/en.png" alt="lang" />EN </a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </nav>
+    <main>
+        @yield('content')
+    </main>
+    <i class="scroll-top-btn scroll-top show"></i>
+    <footer class="light">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-4">
+                    <h3>Execoore</h3>
+                    <p>Full suite enable teams to develop unique search and discovery experiences.</p>
+                    <div class="icon-links icon-social icon-links-grid social-colors">
+                        <a class="facebook"><i class="icon-facebook"></i></a>
+                        <a class="twitter"><i class="icon-twitter"></i></a>
+                        <a class="instagram"><i class="icon-instagram"></i></a>
+                        <a class="pinterest"><i class="icon-pinterest"></i></a>
+                    </div>
+                </div>
+                <div class="col-lg-4">
+                    <h3>Resources</h3>
+                    <ul class="icon-list icon-line">
+                        <li><a href="#">Partners and advertising</a></li>
+                        <li><a href="#">About us and the company</a></li>
+                        <li><a href="#">Services and projects</a></li>
+                        <li><a href="#">Contact us online</a></li>
+                    </ul>
+                </div>
+                <div class="col-lg-4">
+                    <ul class="text-list text-list-line">
+                        <li><b>Address</b><hr /><p>139 Baker St, E1 7PT, London</p></li>
+                        <li><b>Email</b><hr /><p>contacts@example.com</p></li>
+                        <li><b>Phone</b><hr /><p>(02) 123 333 444</p></li>
+                        <li><b>Opening hours</b><hr /><p>8am-5pm Mon - Fri</p></li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+        <div class="footer-bar">
+            <div class="container">
+                <span>© 2019 Execoore - Technology And Business Template Handmade by <a href="https://schiocco.com" target="_blank">schiocco.com</a>.</span>
+                <span><a href="#">Contact us</a> | <a href="#">Privacy policy</a></span>
+            </div>
+        </div>
+        <link rel="stylesheet" href="media/icons/iconsmind/line-icons.min.css">
+        <script src="scripts/jquery.min.js"></script>
+        <script src="scripts/main.js"></script>
+        <script src="scripts/parallax.min.js"></script>
+        <script src="scripts/glide.min.js"></script>
+        <script src="scripts/magnific-popup.min.js"></script>
+        <script src="scripts/tab-accordion.js"></script>
+        <script src="scripts/imagesloaded.min.js"></script>
+        <script src="scripts/progress.js" async></script>
+        <script src="scripts/custom.js" async></script>
+        <script src="scripts/contact-form/contact-form.js" async></script>
+    </footer>
+</body>
+</html>
---
+++ resources/views/inicio.blade.php
@@ -0,0 +1,254 @@
+@extends('home')
+@section('content')
+        <section class="section-image section-home-one no-padding-y" style="background-image:url(media/hd-1.jpg)">
+            <div class="container">
+                <div class="row">
+                    <div class="col-lg-7">
+                        <hr class="space-lg" />
+                        <h3 class="text-color-2">
+                            Pago de servicios próximamente
+                        </h3>
+                        <ul class="slider" data-options="arrows:false,nav:false,autoplay:3000,controls:out">
+                            <li>
+                                <h1 class="text-uppercase">¡Incrementa las ventas de tu negocio!</h1>
+                            </li>
+                        </ul>
+                        <p>
+                            Brinda a tus clientes la facilidad de realizar sus recargas de tiempo aire mediante un sistema, fácil, seguro y con la mejor tecnología. 
+                        </p>
+                        <hr class="space-sm" />
+                        <hr class="space-lg" />
+                        <hr class="space-lg" />
+                    </div>
+                    <div class="col-lg-5 hidden-md">
+                        <hr class="space-sm" />
+                        <img data-anima="fade-bottom" data-time="1000" class="slide-image" src="media/man-1.png" alt="" />
+                    </div>
+                </div>
+            </div>
+        </section>
+        <section class="section-base section-overflow-top">
+            <div class="container">
+                <div class="grid-list" data-columns="4" data-columns-md="2" data-columns-sm="1">
+                    <div class="grid-box">
+                        <div class="grid-item">
+                            <div class="cnt-box cnt-box-top-icon boxed">
+                                <i class="im-monitor-phone"></i>
+                                <div class="caption">
+                                    <h2></h2>
+                                    <p>
+                                        Realiza tus recargas de tiempo aire en todas las compañías telefónicas de México.
+                                    </p>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="grid-item">
+                            <div class="cnt-box cnt-box-top-icon boxed">
+                                <i class="im-bar-chart2"></i>
+                                <div class="caption">
+                                    <h2></h2>
+                                    <p>
+                                        Facil integración, segura y rápida con la mas alta tecnologia <br><br>
+                                    </p>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="grid-item">
+                            <div class="cnt-box cnt-box-top-icon boxed">
+                                <i class=" im-medal"></i>
+                                <div class="caption">
+                                    <h2></h2>
+                                    <p>
+                                        Aumenta tus ingresos con puntos de recarga en tu negocio, con el sistema más actualizado
+                                    </p>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="grid-item">
+                            <div class="cnt-box cnt-box-top-icon boxed">
+                                <i class="im-business-man"></i>
+                                <div class="caption">
+                                    <h2></h2>
+                                    <p>
+                                        Atención personalizada y en momento que lo requieras <br><br>
+                                    </p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="row row-fit-lg" data-anima="fade-bottom" data-time="1000">
+                    <div class="col-lg-6">
+                        <img src="media/grafica.jpg" alt="">
+                    </div>
+                    <div class="col-lg-6">
+                        <div class="title">
+                            <h2>¿Porque te conviene invertir en este negocio?</h2>
+                            <p>Conoce los numeros</p>
+                        </div>
+                        <p>
+                            En 2019, el numero de usuarios de telefonos moviles en México ascendio de 86.46 millones, frente a aproximadamente 42 millones de usuarios en 2009. 
+                        </p>
+                       
+                    </div>
+                </div>
+            </div>
+        </section>
+        <section class="section-base section-color">
+            <div class="container">
+                <div class="row" data-anima="fade-bottom" data-time="1000">
+                    <div class="col-lg-8">
+                        <div class="title">
+                            <h2>Dale un plus a tu negocio e incrementa tus ingresos con nosotros.</h2>
+                            <p>Nuestro Servicios</p>
+                        </div>
+                    </div>
+                </div>
+                <hr class="space" />
+                <div class="grid-list" data-columns="3" data-columns-md="2" data-columns-sm="1" data-anima="fade-bottom" data-time="1000">
+                    <div class="grid-box">
+                        <div class="grid-item">
+                            <div class="cnt-box cnt-box-info boxed" data-href="#">
+                                <a href="#" class="img-box"><img src="media/image-15.jpg" alt="" /></a>
+                                <div class="caption">
+                                    <p>
+                                        Realiza recargas de tiempo aire a tu dispositivo movil de forma segura y rápida. 
+                                    </p>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="grid-item">
+                            <div class="cnt-box cnt-box-info boxed" data-href="#">
+                                <a href="#" class="img-box"><img src="media/image-12.jpg" alt="" /></a>
+                                <div class="caption">
+                                    <p>
+                                        Te brindamos el mayor alcance. Recargas a todas las compañías móviles de México. 
+                                    </p>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="grid-item">
+                            <div class="cnt-box cnt-box-info boxed" data-href="#">
+                                <a href="#" class="img-box"><img src="media/image-2.jpg" alt="" /></a>
+                                <div class="caption">
+                                    <p>
+                                        Realiza el pago de todos tus servicios con nosotros… próximamente.<br><br>
+                                    </p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <section class="section-base">
+            <div class="container">
+                <div class="row align-items-center" data-anima="fade-bottom" data-time="1000">
+                    <div class="col-lg-6">
+                        <div class="title">
+                            <h2>Acerca de nosotros</h2>
+                        </div>
+                        <p>
+                           En Drissly tenemos como objetivo brindar a los usuarios una alternativa conveniente, segura y con la mayor tecnología para realizar sus recargas de tiempo aire a todas las compañias telefónicas del país y proximamente la integración de pagos de servicios públicos, de entretenimiento y municipales. 
+                        </p>
+                        <hr class="space-sm" />
+                    </div>
+                    <div class="col-lg-6">
+                        <img src="media/image-3.jpg" alt="" />
+                    </div>
+                </div>
+            </div>
+        </section>
+        <section class="section-base section-color">
+            <div class="container">
+                <div class="row align-items-center" data-anima="fade-bottom" data-time="1000">
+                    <div class="col-lg-6">
+                        <img src="media/image-6.jpg" alt="" />
+                    </div>
+                    <div class="col-lg-6">
+                        <div class="title">
+                            <h2>Modelo de negocio</h2>
+                        </div>
+                        <p>
+                            El resultado de nuestro modelo de negocio es  un beneficio en conjunto; los usuarios se evitan realizar filas o acudir a ubicaciones remotas para la realización de sus pagos, los proveedores obtienen un alcance de recolección más amplio y los distribuidores  obtienen mayor transito de clientes en sus espacios y mayores ingresos. 
+                        </p>
+                        <hr class="space-sm" />
+                    </div>
+                </div>
+                <hr class="space" />
+                <hr class="space-xs" />
+            </div>
+        </section>
+        <section class="section-image light align-center ken-burn-center" data-parallax="scroll" data-image-src="media/hd-4.jpg">
+            <div class="container" data-anima="fade-bottom" data-time="1000">
+                <hr class="space" />
+                <h2 class="width-750">Contamos con acuerdos comerciales para venta y distribución de recargas de todas las operadoras moviles en México, para brindar. versatilidad en tu negocio, de una forma segura y rapida</h2>
+                <hr class="space" />
+                <table class="table table-grid table-border align-center table-logos table-10">
+                    <tbody>
+                        <tr>
+                            <td>
+                                <img src="media/filetelcel-logosvg-wikimedia-commons-telcel-png-1280_282.png" alt="" />
+                            </td>
+                            <td>
+                                <img src="media/movistar-logo-1.png" alt="" />
+                            </td>
+                            <td>
+                                <img src="media/AT&T_logo_2016.svg" alt="" />
+                            </td>
+                            <td>
+                                <img src="media/1904476.png" alt="" />
+                            </td>
+                            <td>
+                                <img src="media/virgin-mobile-logo-red.png" alt="" />
+                            </td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
+        </section>
+        <section class="section-base">
+            <div class="container">
+                <div class="row" data-anima="fade-bottom" data-time="1000">
+                    <div class="col-lg-6">
+                        <div class="title">
+                            <h2>Escríbenos y nosotros nos podremos en contacto conitgo. </h2>
+                            <p>Contactanos</p>
+                        </div>
+                        <a href="#" class="btn-text active">You accept our policy</a>
+                    </div>
+                    <div class="col-lg-6">
+                        <form id="UCiFI" action="thtmekit/scripts/php/contact-form.php" class="form-box form-ajax form-ajax-wp" method="post" data-email="">
+                            <div class="row">
+                                <div class="col-lg-6">
+                                    <input id="Name" name="Name" placeholder="Nombre" type="text" class="input-text" required="">
+                                </div>
+                                <div class="col-lg-6">
+                                    <input id="Email" name="Email" placeholder="Cargo" type="email" class="input-text" required="">
+                                </div>
+                                <div class="col-lg-6">
+                                    <input id="Name" name="Name" placeholder="Teléfono" type="text" class="input-text" required="">
+                                </div>
+                                <div class="col-lg-6">
+                                    <input id="Email" name="Email" placeholder="Correo" type="email" class="input-text" required="">
+                                </div>
+                            </div>
+                            <hr class="space-xs" />
+                            <textarea id="Message" name="Message" placeholder="Mensaje" class="input-textarea" required=""></textarea>
+                            <button class="btn btn-xs" type="submit">Enviar mensaje</button>
+                            <div class="success-box">
+                                <div class="alert alert-success">
+                                    Congratulations. Your message has been sent successfully.
+                                </div>
+                            </div>
+                            <div class="error-box">
+                                <div class="alert alert-warning">
+                                    Error, please retry. Your message has not been sent.
+                                </div>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </section>
+    @endsection
